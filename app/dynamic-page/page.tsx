411f52233862a327930adfd4aac7aa5f8f219a14
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2025-02-23 16:15:32 +0900

    initial commit

--- app/dynamic-page/page.tsx
+++ app/dynamic-page/page.tsx
@@ -1,11 +1,16 @@
-export default function LocalFontPage() {
+
+'use client';
+
+export const runtime = 'edge';
+
+export default function Page() {
 	return (
 		<div>
 			<p className='font-pretendard text-lg leading-relaxed'>
-				here <strong>Local + next/font</strong> page
+				here <strong>Test</strong> page
 				<br />
 			</p>
-			<p className='font-suit leading-relaxed'>This text should be in SUIT</p>
+			<p className='leading-relaxed'>This text should be in SUIT</p>
 			<p className='font-pretendard leading-relaxed'>
 				This text should be in Pretendard
 			</p>
